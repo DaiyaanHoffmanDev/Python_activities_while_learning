c0282e77357494c923708905400c1a62d350b1cb
DaiyaanHoffmanDev <149784268+DaiyaanHoffmanDev@users.noreply.github.com>
2026-02-20 11:40:56 +0200

    program that greets a guest
    This program greets a guest based on what there status is e.g guest or admin

---
+++ exercisevipgreeter.py
@@ -0,0 +1,6 @@
+def greet(name,status="guest"):
+    print(f"hello {name},you are logged in as {status}")
+
+
+greet("daiyaan")# testing if this works withour another paraminter
+greet("daiyaan","admin")# have to put "" then python doesnt look for variables called daiyaan
